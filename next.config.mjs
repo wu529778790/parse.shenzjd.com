8eca13b64b6e1038eb39ba8344c6d9a9cfb18114
wu529778790 <529778790@qq.com>
2025-06-14 21:09:57 +0800

    在next.config.mjs中添加对i0.hdslb.com的支持，包含HTTPS和HTTP协议配置。

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "**.douyinpic.com",
       },
+      {
+        protocol: "https",
+        hostname: "i0.hdslb.com",
+      },
+      {
+        protocol: "http",
+        hostname: "i0.hdslb.com",
+      },
     ],
   },
 };
